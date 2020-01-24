848572fa1283cd9b3ba42a0c0bead267ff4b1265
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-24 14:40:19 +0530

    form body-parser

--- program.js
+++ program.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser');
 const port = process.argv[2]
 const path = require("path");
 
 
-app.set('view engine', 'pug');
-app.set('views', process.argv[3]);
-app.get('/home', function(req,res) {
-res.render('index', {date: new Date().toDateString()});
-})
+app.use(bodyparser.urlencoded({extended: false}));
+app.post('/form', function(req,res) {
+  res.send(req.body.str.split('').reverse().join(''));
+}) 
+
 
 app.listen(port)
