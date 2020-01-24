f3d06be6bd2484abb079b33266cfe39b9f418352
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-24 11:57:07 +0530

    static operation performed

--- program.js
+++ program.js
@@ -4,5 +4,8 @@ const port = process.argv[2]
 
 app.get('/home',function(req,res){
   res.end('Hello World!')
-})
+ app.use(express.static('public'))
+});
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
 app.listen(port)
