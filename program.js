dd730ab8c7169bc8b74700ee1e1b5d42be0f9ae5
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-24 16:00:07 +0530

    response with JSON

--- program.js
+++ program.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.argv[2]
+const fs = require('fs')
 
-app.get('/search', function (req,res) {
-  const obj = req.query;
-  res.send(obj);
-}) 
-app.listen(port)
+app.get('/books', function(req, res){
+  const book = process.argv[3];
+  fs.readFile(book, function(err, data) {
+    if (err) throw err;
+    const booksObj = JSON.parse(data);
+    res.json(booksObj);
+  })
+})
+app.listen(process.argv[2]);
