a82fd6291c008cf97b9c85baa8cc5e361a6451a5
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-24 14:17:09 +0530

    pug is performed

--- program.js
+++ program.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.argv[2]
+const path = require("path");
 
-app.get('/home',function(req,res){
-  res.end('Hello World!')
- app.use(express.static('public'))
-});
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
+app.set('view engine', 'pug');
+app.set('views', process.argv[3]);
+app.get('/home', function(req,res) {
+res.render('index', {date: new Date().toDateString()});
+})
 
 app.listen(port)
