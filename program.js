99f9539bc2e315f49682c4fe1e86c75f52246f07
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-24 15:15:53 +0530

    from url get the msg

--- program.js
+++ program.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const path = require('path');
+const port = process.argv[2]
 
-const process = require('process')
-
-app.use(require('stylus').middleware(process.argv[3])) 
-app.use(require('stylus').middleware('public'))
-
-app.listen(process.argv[2])
+ app.put('/message/:id', function(req, res){
+  const id = req.params.id
+  const msg = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  res.send(msg)
+}) 
+app.listen(port)
